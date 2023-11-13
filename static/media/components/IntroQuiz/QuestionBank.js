2e08e27f36c505e398d8777a4dac1e1be3029c7d
Phoebe Mueller <phoebe.mueller1@gmail.com>
2023-11-13 13:53:20 -0600

    Merge changes in static folder from main into gh-pages

---
+++ static/media/components/IntroQuiz/QuestionBank.js
@@ -0,0 +1,22 @@
+const qBank = [
+    {
+        id: 1,
+        question: "Find out where to start your Swiftie journey!",
+        options: [],
+        answer: "", // TO DO - figure out how to tally different amounts of points for each answer
+    },
+    {
+        id: 2,
+        question: "What kind of music do you typically listen to?",
+        options: ["Pop", "Rock", "Country", "Rap", "Indie"],
+        answer: "", // TO DO - figure out how to tally different amounts of points for each answer
+    },
+    {
+        id: 3,
+        question: "What is your favorite color?",
+        options: ["Red", "Light Blue", "Forest Green", "Gold", "Pink", "Black"],
+        answer: "",
+    },
+]
+ 
+export default qBank;
